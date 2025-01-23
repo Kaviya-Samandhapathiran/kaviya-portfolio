ec12a743b1dd4d3b725ff90714a4aad216b9ba50
Kaviya-Samandhapathiran <kaviyajain0030@gmail.com>
2025-01-23 22:43:12 +0530

    About section changes

--- script.js
+++ script.js
@@ -11,10 +11,32 @@ closeBar.addEventListener("click", function () {
     navbar.style.display = "none"
 })
 
+//Typed Js
 const typed = new Typed(".section__text__p2",{
     strings : ['Full Stack Developer','Core Java Developer','Front End Developer'],
     typeSpeed: 70,
     backSpeed: 70,
     backdelay: 1000,
     loop: true
+})
+
+//Tab-contents
+
+var experience = document.getElementById("experience")
+var education = document.getElementById("education")
+var expContent = document.getElementById("exp-content")
+var eduContent = document.getElementById("edu-content")
+
+experience.addEventListener("click", function(){
+    experience.classList.add("active-link")
+    expContent.classList.add("active-tab")
+    education.classList.remove("active-link")
+    eduContent.classList.remove("active-tab")
+})
+
+education.addEventListener("click", function(){
+    experience.classList.remove("active-link")
+    expContent.classList.remove("active-tab")
+    education.classList.add("active-link")
+    eduContent.classList.add("active-tab")
 })
